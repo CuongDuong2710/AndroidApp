1c8820b4ab0c5af85982fcb92ac866cd9b3f6777
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-12 12:23:59 +0700

    working with list

--- FireBaseTutorials/app/src/main/java/in/android/cuong/firebasetutorials/GetValueActivity.java
+++ FireBaseTutorials/app/src/main/java/in/android/cuong/firebasetutorials/GetValueActivity.java
@@ -2,38 +2,59 @@ package in.android.cuong.firebasetutorials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class GetValueActivity extends AppCompatActivity {
 
-    private TextView mTextName;
-    private TextView mTextAge;
     private DatabaseReference mDatabase;
+    private ListView mUserList;
+
+    private ArrayList<String> mUsernames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_value);
 
-        // create instance of FirebaseDatabase connect "https://fir-tutorials-1d984.firebaseio.com/Name"
+        // create instance of FirebaseDatabase connect "https://fir-tutorials-1d984.firebaseio.com"
         // remember change Rules of FirebaseDatabase-> ".read": "auth" == null
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Name");
-        mTextName = (TextView) findViewById(R.id.txtName);
-        mTextAge = (TextView) findViewById(R.id.txtAge);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mUserList = (ListView) findViewById(R.id.list_user);
 
-        // Get value from firebase database
-        mDatabase.addValueEventListener(new ValueEventListener() {
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, mUsernames);
+        mUserList.setAdapter(arrayAdapter);
+
+        mDatabase.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String name = dataSnapshot.getValue().toString();
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                mUsernames.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
 
-                mTextName.setText("Name: " + name);
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override

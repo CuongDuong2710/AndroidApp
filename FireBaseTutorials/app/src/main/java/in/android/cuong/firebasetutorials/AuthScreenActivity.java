aa0c63333b248e6dd58afdf7e2342b3cc85c0a71
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-12 14:58:21 +0700

    intent MainActivity

--- FireBaseTutorials/app/src/main/java/in/android/cuong/firebasetutorials/AuthScreenActivity.java
+++ FireBaseTutorials/app/src/main/java/in/android/cuong/firebasetutorials/AuthScreenActivity.java
@@ -1,14 +1,20 @@
 package in.android.cuong.firebasetutorials;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AuthScreenActivity extends AppCompatActivity {
 
     private EditText edtEmail, edtPassword;
     private Button btnAuth;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,14 @@ public class AuthScreenActivity extends AppCompatActivity {
         edtEmail = (EditText) findViewById(R.id.edt_email);
         edtPassword = (EditText) findViewById(R.id.edt_password);
         btnAuth = (Button) findViewById(R.id.btn_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
     }
 }

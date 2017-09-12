132721aa0fdf5e37c7d7ad08b93ea3caeca5b9cd
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-12 10:17:30 +0700

    adding firebase instance

--- FireBaseTutorials/app/src/main/java/in/android/cuong/firebasetutorials/MainActivity.java
+++ FireBaseTutorials/app/src/main/java/in/android/cuong/firebasetutorials/MainActivity.java
@@ -2,12 +2,32 @@ package in.android.cuong.firebasetutorials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFireBasebtn;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mFireBasebtn = (Button) findViewById(R.id.firebase_btn);
+
+        mFireBasebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

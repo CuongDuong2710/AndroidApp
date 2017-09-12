d552f0e82d18e1d8923ac276c238ed012b56bdff
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-12 10:54:12 +0700

    changing rules firebase db and creating edittext

--- FireBaseTutorials/app/src/main/java/in/android/cuong/firebasetutorials/MainActivity.java
+++ FireBaseTutorials/app/src/main/java/in/android/cuong/firebasetutorials/MainActivity.java
@@ -4,13 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mEditName;
     private Button mFireBasebtn;
+
+    private String name = "";
 
     private DatabaseReference mDatabase;
 
@@ -21,11 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        mEditName = (EditText) findViewById(R.id.edt_name);
+
         mFireBasebtn = (Button) findViewById(R.id.firebase_btn);
 
         mFireBasebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // getText from inside click listener, outside is ""
+                name = mEditName.getText().toString().trim();
+
+                // 1 - Create child in root object
+                // 2 - Assign some value to the child object
+
+                mDatabase.child("Name").setValue(name);
 
             }
         });

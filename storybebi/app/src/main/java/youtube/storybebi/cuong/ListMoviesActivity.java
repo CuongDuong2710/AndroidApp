a65a90069b0a877d261b9f0cf6e594ea6ec5583f
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-22 00:03:20 +0700

    inserting ads to movies list

--- storybebi/app/src/main/java/youtube/storybebi/cuong/ListMoviesActivity.java
+++ storybebi/app/src/main/java/youtube/storybebi/cuong/ListMoviesActivity.java
@@ -9,10 +9,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.NativeExpressAdView;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,7 +23,7 @@ import butterknife.ButterKnife;
 public class ListMoviesActivity extends AppCompatActivity {
     @BindView(R.id.list_video) RecyclerView recyclerView;
 
-    private List<HashMap<String, Object>> movies = new ArrayList<>();
+    private List<Object> mRecyclerViewItems = new ArrayList<>();
     private MovieAdapter mAdapter;
 
     @Override
@@ -29,7 +32,12 @@ public class ListMoviesActivity extends AppCompatActivity {
         setContentView(R.layout.list_movies);
         ButterKnife.bind(this);
 
-        mAdapter = new MovieAdapter(ListMoviesActivity.this, movies);
+        // Call before getMovies() to avoid 'NullPointerException' in 'mAdapter.notifyDataSetChanged()'
+        mAdapter = new MovieAdapter(ListMoviesActivity.this, mRecyclerViewItems);
+
+        // get mRecyclerViewItems list from album
+        getMovies();
+        addNativeExpressAds();
 
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
@@ -40,33 +48,40 @@ public class ListMoviesActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view, int position) {
-                // get data record
-                HashMap<String, Object> data = movies.get(position);
-                // set up movie
-                String title = String.valueOf(data.get("title"));
-                String lenght = String.valueOf(data.get("length"));
-                String videoId = String.valueOf(data.get("videoId"));
-                String imageUrl = String.valueOf(data.get("imageUrl"));
-                String description = String.valueOf(data.get("description"));
+                Object item = mRecyclerViewItems.get(position);
+                if (!(item instanceof NativeExpressAdView)) {
+                    // get data record
+                    HashMap<String, Object> data = (HashMap<String, Object>) mRecyclerViewItems.get(position);
+                    // set up movie
+                    String title = String.valueOf(data.get("title"));
+                    String lenght = String.valueOf(data.get("length"));
+                    String videoId = String.valueOf(data.get("videoId"));
+                    String imageUrl = String.valueOf(data.get("imageUrl"));
+                    String description = String.valueOf(data.get("description"));
 
-                Movie movie = new Movie(imageUrl, title, videoId, lenght, description);
+                    Movie movie = new Movie(imageUrl, title, videoId, lenght, description);
 
-                Toast.makeText(getApplicationContext(), movie.getTitle(), Toast.LENGTH_SHORT).show();
-                sendData(movie.getTitle(), movie.getVideoId(), movie.getDescription());
+                    Toast.makeText(getApplicationContext(), movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    sendData(movie.getTitle(), movie.getVideoId(), movie.getDescription());
+                }
             }
 
             @Override
             public void onLongClick(View view, int position) {
 
             }
-
         }));
-
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+    }
 
-        // get movies list from album
-        getMovies();
-
+    private void addNativeExpressAds() {
+        for (int i = 0; i < mRecyclerViewItems.size(); i+=8 ) {
+            NativeExpressAdView adView = new NativeExpressAdView(this);
+            adView.setAdSize(new AdSize(320, 150));
+            adView.setAdUnitId("ca-app-pub-2267083612728588/6133498163");
+            adView.loadAd(new AdRequest.Builder().build());
+            mRecyclerViewItems.add(i, adView);
+        }
     }
 
     /**
@@ -78,7 +93,7 @@ public class ListMoviesActivity extends AppCompatActivity {
         // get list hashmap
         List<HashMap<String, Object>> hashMaps = (List<HashMap<String, Object>>) data.getSerializable("data");
 
-        movies.addAll(hashMaps);
+        mRecyclerViewItems.addAll(hashMaps);
         mAdapter.notifyDataSetChanged();
     }
 
--- storybebi/app/src/main/java/youtube/storybebi/cuong/MovieAdapter.java
+++ storybebi/app/src/main/java/youtube/storybebi/cuong/MovieAdapter.java
@@ -9,10 +9,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.ads.NativeExpressAdView;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,37 +21,75 @@ import butterknife.ButterKnife;
  * Created by QUOC CUONG on 31/08/2017.
  */
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    // A movie item view type
+    private static final int MOVIE_ITEM_VIEW_TYPE = 0;
 
+    // The Native Express view type
+    private static final int AD_VIEW_TYPE = 1;
+
+    // An Activity's context
     private Context mContext;
-    private List<HashMap<String, Object>> movies;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    // The list of Native Express and movie items
+    private List<Object> mRecyclerViewItems;
+
+    /**
+     * The {@Link MovieItemViewHolder} class.
+     * Provides a reference to each view in the movie item view.
+     */
+    public class MovieItemViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.image_video) ImageView image;
         @BindView(R.id.title_video) TextView title;
         @BindView(R.id.duration_video) TextView duration;
 
-        public MyViewHolder(View view) {
+        public MovieItemViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
         }
     }
 
-    public MovieAdapter(Context context, List<HashMap<String, Object>> movies) {
+    public class NativeExpressAdViewHolder extends RecyclerView.ViewHolder {
+        NativeExpressAdViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecyclerViewItems.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position % 8 == 0) ? AD_VIEW_TYPE : MOVIE_ITEM_VIEW_TYPE;
+    }
+
+    public MovieAdapter(Context context, List<Object> movies) {
         this.mContext = context;
-        this.movies = movies;
+        this.mRecyclerViewItems = movies;
     }
 
     /**
-     * inflates movie_list_row.xml
+     * inflates movie_list_row.xml or native_express_ad_cointainer.xml
      * @param parent
      * @param viewType
      * @return
      */
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_list_row, parent, false);
-        return new MyViewHolder(itemView);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case AD_VIEW_TYPE:
+                View nativeExpressLayoutView = LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.native_express_ad_cointainer, parent, false);
+                return new NativeExpressAdViewHolder(nativeExpressLayoutView);
+            case MOVIE_ITEM_VIEW_TYPE:
+            default:
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.movie_list_row, parent, false);
+                return new MovieItemViewHolder(itemView);
+        }
     }
 
     /**
@@ -60,30 +98,55 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder
      * @param position
      */
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        // get hashmap record
-        HashMap<String, Object> data = movies.get(position);
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        switch (viewType) {
+            case AD_VIEW_TYPE:
+                NativeExpressAdViewHolder nativeExpressHolder = (NativeExpressAdViewHolder) holder;
+                NativeExpressAdView adView = (NativeExpressAdView) mRecyclerViewItems.get(position);
 
-        // get each values by key
-        String title = String.valueOf(data.get("title"));
-        String lenght = String.valueOf(data.get("length"));
-        String videoId = String.valueOf(data.get("videoId"));
-        //int position = videoUrl.lastIndexOf("/");
-        //videoUrl.substring(position + 1).trim())
-        String imageUrl = String.valueOf(data.get("imageUrl"));
-        String description = String.valueOf(data.get("description"));
+                // Get a reference to the CardView that going to hold my hand.
+                // This is the CardView from that layout file I made, which is kept as the 'itemView' by the ViewHolder
+                ViewGroup adCardView = (ViewGroup) nativeExpressHolder.itemView;
 
-        // create new Movie
-        Movie movie = new Movie(imageUrl, title, videoId, lenght, description);
+                // And once I've got a hold of that CardView, I can remove any other NativeExpressAdView
+                adCardView.removeAllViews();
 
-        // loading album cover using Glide library
-        Glide.with(mContext).load(movie.getImage()).into(holder.image);
-        holder.title.setText(movie.getTitle());
-        holder.duration.setText(movie.getLength());
-    }
+                // And I can also check the parent of the NativeExpressAdView to make sure it's not
+                // in the hierachy of any of the other cards.
+                if (adView.getParent() != null) {
+                    ((ViewGroup)adView.getParent()).removeView(adView);
+                }
 
-    @Override
-    public int getItemCount() {
-        return movies.size();
+                // And now that my ad is fully detached and I know the CardView is empty, I can pop in the ad.
+                // And my ViewHolder is ready to be returned and viewed by the user.
+                adCardView.addView(adView);
+                break;
+            case MOVIE_ITEM_VIEW_TYPE:
+            default:
+                // cast holder to MovieItemViewHolder
+                MovieItemViewHolder myViewHolder = (MovieItemViewHolder) holder;
+
+                // get hashmap record
+                HashMap<String, Object> data = (HashMap<String, Object>) mRecyclerViewItems.get(position);
+
+                // get each values by key
+                String title = String.valueOf(data.get("title"));
+                String lenght = String.valueOf(data.get("length"));
+                String videoId = String.valueOf(data.get("videoId"));
+                //int position = videoUrl.lastIndexOf("/");
+                //videoUrl.substring(position + 1).trim())
+                String imageUrl = String.valueOf(data.get("imageUrl"));
+                String description = String.valueOf(data.get("description"));
+
+                // create new Movie
+                Movie movie = new Movie(imageUrl, title, videoId, lenght, description);
+
+                // add movie details to the movie item view
+                Glide.with(mContext).load(movie.getImage()).into(myViewHolder.image); // loading album cover using Glide library
+                myViewHolder.title.setText(movie.getTitle());
+                myViewHolder.duration.setText(movie.getLength());
+                break;
+        }
     }
 }

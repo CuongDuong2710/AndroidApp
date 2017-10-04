bef69cb0f9638ef4b09c0c64fcbbdccc3ec304ac
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-04 10:58:39 +0700

    creating BaseActivity class

--- Mvp_Rx_Retrofit_Dagger2/app/src/main/java/android/cuong/mvp_rx_retrofit_dagger2/BaseActivity.java
+++ Mvp_Rx_Retrofit_Dagger2/app/src/main/java/android/cuong/mvp_rx_retrofit_dagger2/BaseActivity.java
@@ -1,6 +1,7 @@
 package android.cuong.mvp_rx_retrofit_dagger2;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
@@ -10,12 +11,36 @@ import android.support.v7.app.AppCompatActivity;
  * Created by QUOC CUONG on 04/10/2017.
  */
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     private ProgressDialog mProgressDialog;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(getContentView());
+        onViewReady(savedInstanceState, getIntent());
+    }
+
+    protected abstract int getContentView();
+
+    protected void onViewReady(Bundle savedInstanceState, Intent intent) {
+        // To be used by child activities
+    }
+
+    protected void showDialog(String message) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setCancelable(true);
+        }
+        mProgressDialog.setMessage(message);
+        mProgressDialog.show();
+    }
+
+    protected void hideDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 }

8c3e943c9b47e661aae10d1c0dc976b94cfba007
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-04 11:41:02 +0700

    completing CakeApiService

--- Mvp_Rx_Retrofit_Dagger2/app/src/main/java/android/cuong/mvp_rx_retrofit_dagger2/api/CakeApiService.java
+++ Mvp_Rx_Retrofit_Dagger2/app/src/main/java/android/cuong/mvp_rx_retrofit_dagger2/api/CakeApiService.java
@@ -1,5 +1,8 @@
 package android.cuong.mvp_rx_retrofit_dagger2.api;
 
+import android.cuong.mvp_rx_retrofit_dagger2.mvp.model.CakesResponse;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 import rx.Observable;
 
@@ -12,5 +15,8 @@ import rx.Observable;
 public interface CakeApiService {
 
     @GET("/filippella/a728a34822a3bc7add98e477a4057b69/raw/310d712e87941f569074a63fedb675d2b611342a/cakes")
-    Observable<>
+    Observable<CakesResponse> getCakes();
+
+    @GET("/filippella/a728a34822a3bc7add98e477a4057b69/raw/310d712e87941f569074a63fedb675d2b611342a/cakes")
+    Call<CakesResponse> getTheCakes();
 }

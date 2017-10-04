a16c26bbd7e7bb2316bd81fd1087271ddd32a0a0
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-04 11:05:06 +0700

    creating CakeApplication class

---
+++ Mvp_Rx_Retrofit_Dagger2/app/src/main/java/android/cuong/mvp_rx_retrofit_dagger2/application/CakeApplication.java
@@ -0,0 +1,21 @@
+package android.cuong.mvp_rx_retrofit_dagger2.application;
+
+import android.app.Application;
+
+/**
+ * Created by QUOC CUONG on 04/10/2017.
+ * Application is a single object which will be available throughout the entire lifecycle of app
+ */
+
+public class CakeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}

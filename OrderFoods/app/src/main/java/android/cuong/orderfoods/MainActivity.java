6975bd353839ea2cf00cbc196cf08068ebf4a895
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-06 09:03:26 +0700

    adding library and creating layout

--- OrderFoods/app/src/main/java/android/cuong/orderfoods/MainActivity.java
+++ OrderFoods/app/src/main/java/android/cuong/orderfoods/MainActivity.java
@@ -2,12 +2,22 @@ package android.cuong.orderfoods;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btnSignUp) Button btnSignUp;
+    @BindView(R.id.btnSignIn) Button btnSignIn;
+    @BindView(R.id.txtSlogan) TextView txtSlogan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }

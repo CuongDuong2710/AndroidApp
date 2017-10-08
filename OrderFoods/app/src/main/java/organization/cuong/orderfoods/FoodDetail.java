d9903351c66498505bb2b367733a36bd02c684d1
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-08 14:45:29 +0700

    binding data to foods details

--- OrderFoods/app/src/main/java/organization/cuong/orderfoods/FoodDetail.java
+++ OrderFoods/app/src/main/java/organization/cuong/orderfoods/FoodDetail.java
@@ -1,14 +1,88 @@
 package organization.cuong.orderfoods;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.cuong.orderfoods.R;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import organization.cuong.orderfoods.Model.Food;
 
 public class FoodDetail extends AppCompatActivity {
+    @BindView(R.id.img_food) ImageView food_image;
+    @BindView(R.id.food_name) TextView food_name;
+    @BindView(R.id.food_price) TextView food_price;
+    @BindView(R.id.food_description) TextView food_description;
+    @BindView(R.id.collapsing) CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.btnCart) FloatingActionButton btnCart;
+    @BindView(R.id.number_button) ElegantNumberButton numberButton;
+
+    String foodId = "";
+
+    FirebaseDatabase database = null;
+    DatabaseReference foods = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
+        ButterKnife.bind(this);
+
+        // Init FireBase
+        database = FirebaseDatabase.getInstance();
+        foods = database.getReference("Foods");
+
+        // setting collapsing toolbar layout
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandeddAppbar);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppbar);
+
+        // getting FoodId from FoodList
+        if (getIntent() != null) {
+            foodId = getIntent().getStringExtra("FoodId");
+        }
+        // loading food details
+        if (!foodId.isEmpty()) {
+            getDetailFood(foodId);
+        }
+    }
+
+    /**
+     * setting food details (image, name, price, description)
+     * @param foodId
+     */
+    private void getDetailFood(String foodId) {
+        foods.child(foodId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Food food = dataSnapshot.getValue(Food.class);
+
+                // setting image
+                Picasso.with(getBaseContext()).load(food.getImage())
+                        .into(food_image);
+
+                // setting components
+                collapsingToolbarLayout.setTitle(food.getName());
+                food_name.setText(food.getName());
+                food_price.setText(food.getPrice());
+                food_description.setText(food.getDescription());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- OrderFoods/app/src/main/java/organization/cuong/orderfoods/FoodList.java
+++ OrderFoods/app/src/main/java/organization/cuong/orderfoods/FoodList.java
@@ -1,5 +1,6 @@
 package organization.cuong.orderfoods;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.cuong.orderfoods.R;
@@ -81,7 +82,10 @@ public class FoodList extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(FoodList.this, "" + currentFoodItem.getName(), Toast.LENGTH_SHORT).show();
+                        // send FoodId to FoodDetail
+                        Intent foodDetail = new Intent(FoodList.this, FoodDetail.class);
+                        foodDetail.putExtra("FoodId", adapter.getRef(position).getKey());
+                        startActivity(foodDetail);
                     }
                 });
             }

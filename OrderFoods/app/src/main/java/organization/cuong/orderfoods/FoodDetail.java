99bc7046c7d1a36d9ec196e32c0b7e903c9ec0ea
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-08 12:17:04 +0700

    adding library and FoodDetail activity

---
+++ OrderFoods/app/src/main/java/organization/cuong/orderfoods/FoodDetail.java
@@ -0,0 +1,14 @@
+package organization.cuong.orderfoods;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.cuong.orderfoods.R;
+
+public class FoodDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_detail);
+    }
+}

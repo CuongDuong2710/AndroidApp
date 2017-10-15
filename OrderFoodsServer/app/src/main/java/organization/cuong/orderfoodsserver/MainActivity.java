b7f2a30202203e1bf2b3c4a24b5fcc9a85db1f17
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-15 15:59:32 +0700

    adding library and setting type face

--- OrderFoodsServer/app/src/main/java/organization/cuong/orderfoodsserver/MainActivity.java
+++ OrderFoodsServer/app/src/main/java/organization/cuong/orderfoodsserver/MainActivity.java
@@ -1,13 +1,27 @@
 package organization.cuong.orderfoodsserver;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btnSignIn) Button btnSignIn;
+    @BindView(R.id.txtSlogan) TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // set typeface
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
     }
 }

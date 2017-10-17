e1ee8d44b09ceafda89b14ee43cb14f7275eb547
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-17 22:35:00 +0700

    creating dialog to uploading category

--- OrderFoodsServer/app/src/main/java/organization/cuong/orderfoodsserver/Home.java
+++ OrderFoodsServer/app/src/main/java/organization/cuong/orderfoodsserver/Home.java
@@ -1,10 +1,17 @@
 package organization.cuong.orderfoodsserver;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,12 +25,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import info.hoang8f.widget.FButton;
 import organization.cuong.orderfoodsserver.Common.Common;
 import organization.cuong.orderfoodsserver.Interface.ItemClickListener;
 import organization.cuong.orderfoodsserver.Model.Category;
@@ -32,16 +49,29 @@ import organization.cuong.orderfoodsserver.ViewHolder.MenuViewHolder;
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     @BindView(R.id.recycler_menu) RecyclerView recycler_menu;
+    MaterialEditText edtName;
+    FButton btnUpload;
+    FButton btnSelect;
 
     TextView txtFullName;
 
     // Firebase
     FirebaseDatabase database = null;
     DatabaseReference categories = null;
+    FirebaseStorage storage = null;
+    StorageReference storageReference = null;
     FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter = null;
 
     // View
     RecyclerView.LayoutManager mLayoutManager = null;
+
+    // New category
+    Category newCategory = null;
+
+    // Uri for saving image
+    Uri saveUri = null;
+
+    private final int PICK_IMAGE_REQUEST = 71;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,13 +86,15 @@ public class Home extends AppCompatActivity
         // init Firebase
         database = FirebaseDatabase.getInstance();
         categories = database.getReference("Category");
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
+        // Click on Floating Action button to add new category
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showDialog();
             }
         });
 
@@ -89,6 +121,143 @@ public class Home extends AppCompatActivity
         loadMenu();
     }
 
+    /**
+     * Adding new category
+     */
+    private void showDialog() {
+        // set title and message
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Home.this);
+        alertDialog.setTitle("Add new Category");
+        alertDialog.setMessage("Please fill full information");
+
+        // inflating layout
+        LayoutInflater inflater = this.getLayoutInflater();
+        View add_menu_layout = inflater.inflate(R.layout.add_new_menu_layout, null);
+
+        edtName = add_menu_layout.findViewById(R.id.edtName);
+        btnUpload = add_menu_layout.findViewById(R.id.btnUpload);
+        btnSelect = add_menu_layout.findViewById(R.id.btnSelect);
+
+        // set layout and icon for dialog
+        alertDialog.setView(add_menu_layout);
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+        // event for button
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
+
+        // set button
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                // Here, just create new Category
+                if (newCategory != null) {
+                    categories.push().setValue(newCategory);
+                }
+            }
+        });
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+
+    /**
+     * Uploading image to Firebase Storage
+     */
+    private void uploadImage() {
+        if (saveUri != null) {
+            final ProgressDialog mDialog = new ProgressDialog(this);
+            mDialog.setMessage("Uploading...");
+            mDialog.show();
+
+            // set image name
+            String imageName = UUID.randomUUID().toString();
+            // set folder
+            final StorageReference imageFolder = storageReference.child("images/" + imageName);
+            // uploading image to folder
+            imageFolder.putFile(saveUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            mDialog.dismiss();
+                            Toast.makeText(Home.this, "Uploaded!!!", Toast.LENGTH_SHORT).show();
+                            // set value for new Category if image uploaded
+                            // and we can get download link
+                            imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    newCategory = new Category(edtName.getText().toString(),
+                                                    uri.toString());
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mDialog.dismiss();
+                            Toast.makeText(Home.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            // don't worry about this error
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() /
+                                    taskSnapshot.getTotalByteCount());
+                            mDialog.setMessage("Uploaded " + progress + "%");
+                        }
+                    });
+        }
+    }
+
+
+    /**
+     * Get result from choose image. Press Ctrl + O to open this function
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            // get Uri from image selected
+            saveUri = data.getData();
+            btnSelect.setText("Image Selected!");
+        }
+    }
+
+    /**
+     * Letting user selects image from Gallery and save Uri for image
+     */
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    /**
+     * Loading category foods
+     */
     private void loadMenu() {
         // setting adapter
         adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(Category.class,

40cdbe1950786b621baa9f6ab613966571d7c1cf
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-20 11:08:25 +0700

    loading food list

--- OrderFoodsServer/app/src/main/java/organization/cuong/orderfoodsserver/FoodList.java
+++ OrderFoodsServer/app/src/main/java/organization/cuong/orderfoodsserver/FoodList.java
@@ -1,13 +1,99 @@
 package organization.cuong.orderfoodsserver;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import organization.cuong.orderfoodsserver.Interface.ItemClickListener;
+import organization.cuong.orderfoodsserver.Model.Food;
+import organization.cuong.orderfoodsserver.ViewHolder.FoodViewHolder;
 
 public class FoodList extends AppCompatActivity {
+    @BindView(R.id.recycler_food) RecyclerView recyclerView;
+    @BindView(R.id.add_new_food) FloatingActionButton fab;
+
+    RecyclerView.LayoutManager layoutManager = null;
+
+    // Firebase
+    FirebaseDatabase db = null;
+    DatabaseReference foodList = null;
+    FirebaseStorage storage = null;
+    StorageReference storageReference = null;
+
+    String categoryId = "";
+
+    FirebaseRecyclerAdapter<Food, FoodViewHolder> adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_list);
+        ButterKnife.bind(this);
+
+        // Firebase
+        db = FirebaseDatabase.getInstance();
+        foodList = db.getReference("Foods");
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        // set recyclerview
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // click on floating action button
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // get data from menu item
+        if (getIntent() != null) {
+            categoryId = getIntent().getStringExtra("CategoryId");
+        }
+        if (!categoryId.isEmpty()) {
+            loadListFood(categoryId);
+        }
+    }
+
+    /**
+     * Loding food list
+     * @param categoryId
+     */
+    private void loadListFood(String categoryId) {
+        adapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(
+                Food.class,
+                R.layout.food_item,
+                FoodViewHolder.class,
+                foodList.orderByChild("menuId").equalTo(categoryId)) {
+            @Override
+            protected void populateViewHolder(FoodViewHolder viewHolder, Food model, int position) {
+                viewHolder.txtFoodName.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage())
+                        .into(viewHolder.imageFood);
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+
+                    }
+                });
+            }
+        };
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
     }
 }
